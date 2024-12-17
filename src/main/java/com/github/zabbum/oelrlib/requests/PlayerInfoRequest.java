f3e1775ce9f1ae87749d9646174c1f03be0140df
Paweł Kubas <pawel.kubas.jan@gmail.com>
2024-12-17 19:14:52 +0100

    AllArgsConstructor and NoArgsConstructor in OelRequests

--- src/main/java/com/github/zabbum/oelrlib/requests/PlayerInfoRequest.java
+++ src/main/java/com/github/zabbum/oelrlib/requests/PlayerInfoRequest.java
@@ -1,8 +1,12 @@
 package com.github.zabbum.oelrlib.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 public class PlayerInfoRequest implements OelRequest {
